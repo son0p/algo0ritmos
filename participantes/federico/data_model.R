3d993f14e5890c42d3964dee5e9f47e482a45630
son0p <fede2001@gmail.com>
2022-01-06 06:10:15 -0500

    P: single data model without comparison
    S: Publisher-Subscriber dummy model initiated

--- participantes/federico/data_model.R
+++ participantes/federico/data_model.R
@@ -1,5 +1,6 @@
 devtools::install_github("bergant/datamodelr")
 if(!require(DiagrammeR)){install.packages("DiagrammeR")}
+
 library(datamodelr)
 library(DiagrammeR)
 options(browser = "brave")
@@ -7,8 +8,6 @@ options(browser = "brave")
 #file_path <- system.file("~/builds/algo0ritmos/participantes/federico/data_model_YAML.yml")
 #dm <- datamodelr::dm_read_yaml(file_path)
 
-##A data model not array, relational.how to traverse
-##ledger?
 dm <-
     dm_read_yaml(text = "
 # data model segments
@@ -16,10 +15,10 @@ dm <-
 - segment: &Coo Coordination
 - segment: &Inter Interpretation
 # Tables and columns
-- table: Intruments
+- table: Instruments
   segment: *Inter
   columns:
-    Intrument ID: {key: yes}
+    Instrument ID: {key: yes}
     Filter:
     ADRS:
 - table: Modifiers
@@ -45,8 +44,8 @@ dm <-
   segment: *Gen
   columns:
     Euclidean ID: {key: yes}
-    Config: {ref: Intrument}
-    Alter config: {ref: Intrument}
+    Config: {ref: Instruments}
+    Alter config: {ref: Instruments}
     Order date:
     Requested ship date:
     Status:
@@ -70,19 +69,11 @@ dm <-
   segment: *Coo
   columns:
     Mood ID: {key: yes}
-    Join: {ref: Intruments}
-    Alter join: {ref: Intruments}
+    Join: {ref: Instruments}
+    Alter join: {ref: Instruments}
     Order date:
     Requested ship date:
     Status:
-- table: Parts
-  segment: *Coo
-  columns:
-    Part ID: {key: yes, ref: Order}
-    Line number: {key: yes}
-    Order item: {ref: arrays}
-    Quantity:
-    Price:
 - table: Arrays
   segment: *Coo
   display: accent2
@@ -97,12 +88,89 @@ dm <-
 - table: Players
   segment: *Inter
   columns:
-    Player ID: {key: yes, ref: Order}
+    Player ID: {key: yes}
     Line number: {key: yes}
 ")
+
 dm <- datamodelr::as.data_model(dm)
-dm_dvdrental_seg <- dm_set_segment(dm, table_segments)
 graph <- dm_create_graph(dm, rankdir = "RL", view_type = "keys_only")
-dm_render_graph(graph)
+fig_array_data_model <- dm_render_graph(graph)
 
-
+dm_functional <-
+    dm_read_yaml(text = "
+# data model segments
+- segment: &Top Topic
+- segment: &Pub Publish
+- segment: &Sub Subscriber
+# Tables and columns
+- table: Instruments
+  segment: *Sub
+  columns:
+    Instrument ID: {key: yes}
+    Filter:
+    ADRS:
+    NotePitch: {ref: Selected_from_Equations}
+    TurnOn: {ref: Euclideans}
+- table: Modifiers
+  segment: *Sub
+  columns:
+    Modifier ID: {key: yes}
+    Filter:
+    ADRS: {ref: Selected_from_Equations}
+- table: Inspirations
+  segment: *Top
+  columns:
+    Inspiration ID: {key: yes}
+    Instrument:
+    mood: {ref: moods}
+    Fram: {ref: Now}
+- table: Euclideans
+  segment: *Top
+  columns:
+    Euclidean ID: {key: yes}
+    Config: {ref: liveCode}
+    Alter config: {ref: liveCode}
+    Order date:
+    Requested ship date:
+    Status:
+    Larry: {ref: Now}
+- table: Selected_from_Equations
+  segment: *Top
+  columns:
+    equationsID: {key: yes}
+    ecuation:
+    latex:
+    Status:
+    Larry: {ref: Now}
+- table: Conterpoints
+  segment: *Top
+  columns:
+    Contrapunto ID: {key: yes}
+    Contrapunto law:
+    Status:
+    Larry: {ref: Now}
+- table: Pattern
+  segment: *Pub
+  columns:
+    PatternID: {key: yes}
+    Status:
+- table: Parts
+  segment: *Pub
+  columns:
+    Part ID: {key: yes}
+    TurnOn item: {ref: Now}
+- table: Now
+  segment: *Pub
+  display: accent2
+  columns:
+    Frame: {key: yes}
+    Description:
+    Category:
+    Size:
+    Color:
+    Pattern: {ref: Pattern}
+")
+dm_functional <- datamodelr::as.data_model(dm_functional)
+graph <- dm_create_graph(dm_functional, rankdir = "RL", view_type = "keys_only")
+fig_functional_data_model <- dm_render_graph(graph)
+##fig_functional_data_model
