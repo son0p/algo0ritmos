56b89b81709b7969ee601e4e24d9bc9f25ef2970
Federico Lopez <fede2001@gmail.com>
2022-03-20 18:13:31 -0500

    P: Inexistent code for testing  OSC using lisp
    S: 16 floats successfully transmitted

--- participantes/federico/OSC.lisp
+++ participantes/federico/OSC.lisp
@@ -1,15 +1,75 @@
-(load "~/quicklisp/setup.lisp")
-(ql:quickload "iolib")
-(ql:quickload "inferior-shell")
+(ql:quickload :osc)
+(ql:quickload :usocket)
+; (ql:quickload "usocket")
+;(ql:quickload "inferior-shell")
 
-(require 'inferior-shell)
+(defun osc-send-test (host port)
+  "a basic test function which sends osc test message to a given port/hostname.
+  note ip#s need to be in the format #(127 0 0 1) for now.. ."
+  (let ((s (USOCKET:socket-connect host port
+                           :protocol :datagram
+                           :element-type '(unsigned-byte 8)))
+        (b (osc:encode-message "/foo/bar" "baz" 1 2 3 (coerce PI 'single-float))))
+    (format t "sending to ~a on port ~A~%~%" host port)
+    (unwind-protect
+ (USOCKET:socket-send s b (length b))
+      (when s (USOCKET:socket-close s)))))
 
-(inferior-shell:run "oscchief send 127.0.0.1 6448 /audio/1/foo  ffffffffffffffff 0.222 0.111 0.333 0.444 1 1 1 1 1 1 1 1 1 1 1 1"
-                             :on-error nil
-                             :error-output :string
-                             :output :string)
+(osc-send-test #(127 0 0 1) 6668)
 
-(inferior-shell:run "oscchief send 127.0.0.1 6448 /audio/1/foo  ffffffffffffffff 30.0 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45"
-                             :on-error nil
-                             :error-output :string
-                             :output :string)
+(defun osc-receive-test (port)
+  "a basic test function which attempts to decode an osc message on given port.
+  note ip#s need to be in the format #(127 0 0 1) for now.. ."
+  (let ((s (socket-connect nil nil
+                           :local-port port
+                           :local-host #(127 0 0 1)
+                           :protocol :datagram
+                           :element-type '(unsigned-byte 8)))
+        (buffer (make-sequence '(vector (unsigned-byte 8)) 1024)))
+    (format t "listening on localhost port ~A~%~%" port)
+    (unwind-protect
+         (loop do
+           (socket-receive s buffer (length buffer))
+           (format t "received -=> ~S~%" (osc:decode-bundle buffer)))
+      (when s (socket-close s)))))
+
+(osc-receive-test 6667)
+
+(socket-connect #(127 0 0 1) 6668
+                           :protocol :datagram
+                           :element-type '(unsigned-byte 8))
+
+;; tested with simpleOSCpattern.ck --------------------
+
+(defun osc-send-test1 (host port list)
+  "a basic test function which sends osc test message to a given port/hostname.
+  note ip#s need to be in the format #(127 0 0 1) for now.. ."
+  (let ((s (USOCKET:socket-connect host port
+                           :protocol :datagram
+                           :element-type '(unsigned-byte 8)))
+        (b (osc:encode-message "/audio/2/bass"  110.0 0.0 0.0 0.0 680.0 0.0 0.0 0.0 100.0 0.0 0.0 0.0 100.0 0.0 0.0 0.0  )))
+    (format t "sending to ~a on port ~A~%~%" host port)
+    (unwind-protect
+ (USOCKET:socket-send s b (length b))
+      (when s (USOCKET:socket-close s)))))
+
+(osc-send-test1 #(127 0 0 1) 6450 (list 1 2 3 4))
+
+;; ===========================================================================
+;; pool trying to conform osc:encode-message
+(setq *osc-message* '(list '("/foo/bar" "baz" 1 2 3 (coerce PI 'single-float))))
+(let test '((string "/foo/bar") "baz" 1 2 3 (COERCE PI 'SINGLE-FLOAT)))
+(type-of(car test)) ;; firt element
+(type-of(intern (string (car test))))
+
+(let ((str "/foo/bar")
+      (str2 "baz"))
+  (str str2))
+
+(let ((str "Hello, world!"))
+  (string-upcase str))
+
+(let ((str1 "Hello, world!")
+      (str2 "me too"))
+  (string-downcase (append '(str1 str2))))
+
--- participantes/federico/simpleOSCpattern.ck
+++ participantes/federico/simpleOSCpattern.ck
@@ -1,26 +1,32 @@
 // first run initialize.ck
-//
+// tested with  OSC.lisp
 
 Library lib;
 Global glo;
 OSC_Read osc;
 OscIn oin;
 OscMsg msg;
+6450 => oin.port;
+oin.addAddress( "/audio/2/bass, ffffffffffffffff" );
 
-// osc data to array
- fun void oscRxFloat(){
-        while(true){
-            // wait for event to arrive
-            oin => now;
-            while (oin.recv(msg)){
-                for(0 => int i; i < 16; i++){
-                    msg.getFloat(i) => Global.bassFromOsc[i];
-                }
-            }
-        }
-    }
+// osc data to array --------------------
+ fun void oscRxFloat()
+ {
+     while(true)
+     {
+         // wait for event to arrive
+         oin => now;
+         while (oin.recv(msg))
+         {
+             for(0 => int i; i < 16; i++)
+             {
+                 msg.getFloat(i) => Global.bassFromOsc[i];
+             }
+         }
+     }
+ }
 
-// play some instrument
+// play some instrument --------------------
 fun void playBassFromOsc()
 {
     while(true)
@@ -29,13 +35,14 @@ fun void playBassFromOsc()
         lib.bass.keyOff();
         if( bassNote != -1 ){ Std.mtof(bassNote) => lib.fat.freq; lib.bass.keyOn();  }
         Global.beat => now;
-        <<< bassNote >>>;
-       }
+    }
 }
 
-// tickers
-fun void rollCounter(){
-        while(true){
+// tickers --------------------
+fun void rollCounter()
+{
+    while(true)
+    {
         Global.counter  +  1 @=> Global.counter;
         Global.counter %   4 @=> Global.mod4;
         Global.counter %  16 @=> Global.mod16;
@@ -47,11 +54,12 @@ fun void rollCounter(){
     }
 }
 
-//// SPORKS
+//// SPORKS --------------------
 spork~ playBassFromOsc();
 spork~ rollCounter();
+spork~ oscRxFloat();
 
 // keep sporks alive
-Global.beat * 256 => now;
+Global.beat * 16 => now;
 // reload me
-Machine.add(me.dir()+"simpleOSCpattern.ck:777");
+Machine.add(me.dir()+"simpleOSCpattern.ck");
